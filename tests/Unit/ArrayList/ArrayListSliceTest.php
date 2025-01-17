294b29fce3f75f7b4915c93521cb4cdeb9d34dd2
Thiago Cordeiro <thiagoguetten@gmail.com>
2025-01-17 09:50:47 +0100

    Added sort and slice functions

---
+++ tests/Unit/ArrayList/ArrayListSliceTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tcds\Io\Generic\Unit\ArrayList;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Tcds\Io\Generic\ArrayList;
+use Tcds\Io\Generic\Fixtures\Bar;
+
+class ArrayListSliceTest extends TestCase
+{
+    #[Test] public function given_an_array_list_when_the_offset_is_positive_then_get_a_sliced_array_list(): void
+    {
+        $list = new ArrayList([
+            new Bar("1"),
+            new Bar("2"),
+            new Bar("3"),
+            new Bar("4"),
+            new Bar("5"),
+            new Bar("6"),
+        ]);
+
+        $slice = $list->slice(2, 2);
+
+        $this->assertEquals(
+            new ArrayList([
+                new Bar("3"),
+                new Bar("4"),
+            ]),
+            $slice,
+        );
+    }
+
+    #[Test] public function given_an_array_list_when_the_offset_is_negative_then_get_a_sliced_array_list(): void
+    {
+        $list = new ArrayList([
+            new Bar("1"),
+            new Bar("2"),
+            new Bar("3"),
+            new Bar("4"),
+            new Bar("5"),
+            new Bar("6"),
+        ]);
+
+        $slice = $list->slice(-2, 2);
+
+        $this->assertEquals(
+            new ArrayList([
+                new Bar("5"),
+                new Bar("6"),
+            ]),
+            $slice,
+        );
+    }
+}
---
+++ tests/Unit/ArrayList/ArrayListSortTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tcds\Io\Generic\Unit\ArrayList;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Tcds\Io\Generic\ArrayList;
+use Tcds\Io\Generic\Fixtures\Bar;
+
+class ArrayListSortTest extends TestCase
+{
+    #[Test] public function given_an_array_list_then_sort_without_comparing_callback(): void
+    {
+        $list = new ArrayList([
+            new Bar("4"),
+            new Bar("2"),
+            new Bar("1"),
+            new Bar("3"),
+        ]);
+
+        $sorted = $list->sort();
+
+        $this->assertEquals(new ArrayList([new Bar("4"), new Bar("2"), new Bar("1"), new Bar("3")]), $list);
+        $this->assertEquals(new ArrayList([new Bar("1"), new Bar("2"), new Bar("3"), new Bar("4")]), $sorted);
+    }
+
+    #[Test] public function given_an_array_list_then_sort_with_comparing_callback(): void
+    {
+        $list = new ArrayList([
+            new Bar("44"),
+            new Bar("26"),
+            new Bar("17"),
+            new Bar("35"),
+        ]);
+
+        $sorted = $list->sort(fn(Bar $a, Bar $b) => intval(strrev($a->value)) - intval(strrev($b->value)));
+
+        $this->assertEquals(new ArrayList([new Bar("44"), new Bar("26"), new Bar("17"), new Bar("35")]), $list);
+        $this->assertEquals(new ArrayList([new Bar("44"), new Bar("35"), new Bar("26"), new Bar("17")]), $sorted);
+    }
+}
